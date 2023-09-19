934c58d37386167278652903c5e7e66b958ea276
srs-sieam <srssieam@gmail.com>
2023-09-19 08:05:58 +0600

    use Link instead of <a> to prevent loading

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,13 +1,17 @@
-
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h2>Navbar</h2>
             <nav>
-                <a href="/">Home</a>
+                {/* <a href="/">Home</a>
                 <a href="/about">About</a>
-                <a href="/contact">Contact us</a>
+                <a href="/contact">Contact us</a> */}
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact us</Link>
             </nav>
         </div>
     );
