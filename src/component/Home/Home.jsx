05cc139e43913b0b036eb0463f5c31ead6fa635f
srs-sieam <srssieam@gmail.com>
2023-09-20 07:12:57 +0600

    find location using useLocation hook

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import LoadingPage from "../LoadingPage/LoadingPage";
@@ -6,6 +6,8 @@ import LoadingPage from "../LoadingPage/LoadingPage";
 
 const Home = () => {
     const navigaiton = useNavigation();
+    const location = useLocation();
+    console.log(location);
     return (
         <div>
             <Header></Header>
